560231a87693a94f9ac9904db6cdcfb9e73d2693
kookxiang <r18@ikk.me>
2015-05-06 14:41:25 +0800

    Chinese-Simplified translation

--- EventMapHpViewer/MapHpViewer.cs
+++ EventMapHpViewer/MapHpViewer.cs
@@ -6,8 +6,8 @@ using Grabacr07.KanColleViewer.Composition;
 namespace EventMapHpViewer
 {
     [Export(typeof(IToolPlugin))]
-    [ExportMetadata("Title", "MapHPViewer")]
-    [ExportMetadata("Description", "Map HPを表示します。")]
+    [ExportMetadata("Title", "海域血量查看器")]
+    [ExportMetadata("Description", "显示当前海域的血量")]
     [ExportMetadata("Version", "2.2.1")]
     [ExportMetadata("Author", "@veigr")]
     public class MapHpViewer : IToolPlugin
@@ -21,7 +21,7 @@ namespace EventMapHpViewer
 
         public string ToolName
         {
-            get { return "MapHP"; }
+            get { return "海域血量"; }
         }
 
         public object GetSettingsView()
